fe96b76335101a6add4ae85f4acdb21f820f03e1
jflape <jflape@davao-water.gov.ph>
2023-06-23 15:14:06 +0800

    23JunV2

--- socweb/file_upload/views.py
+++ socweb/file_upload/views.py
@@ -8,7 +8,7 @@ def upload_file(request):
     df = []
     if request.method == 'POST':
         file = request.FILES['file']
-        directory = '../output/'
+        directory = './output/'
 
         df.append(pd.read_excel(file, usecols=['IP Address']))
         df_data = pd.concat(df, axis=0)
@@ -26,14 +26,41 @@ def upload_file(request):
         with open(file_path, 'a') as f:
             f.write(df_txtoutput + '\n')
         
+        with open(file_path, 'r') as rm_dup:
+            dup_outs = rm_dup.readlines()
+            
+        unique_lines = []
+        num_duplicate_lines = 0
+        
+        for dup_out in dup_outs:
+            dup_out = dup_out.strip()
+            
+            if dup_out not in unique_lines:
+                unique_lines.append(dup_out)
+            else:
+                num_duplicate_lines += 1
+        
+        num_lines_inserted = len(unique_lines) - len(dup_outs)
+                
+        with open(file_path,'w') as no_dups:
+            no_dups.write('\n'.join(unique_lines) + '\n')
+        
         with open(file_path, 'r') as showdata:
-            contents = showdata.read()
+            alldata = showdata.read()
+        
+        contents = {
+            'alldata' : alldata,
+            'num_lines_inserted' : num_lines_inserted,
+            'num_duplicate_lines' : num_duplicate_lines
+        }    
 
-        return render(request, 'file_upload/result.html', {'display': contents})
+        # return render(request, 'file_upload/result.html', {'display': contents})
+        return render(request, 'file_upload/result.html', contents)
+    
     return render(request, 'file_upload/upload.html')
 
 def output_data(request):
-        directory = '../output/'
+        directory = './output/'
         file_name = "output.txt"
     #   file_path = directory + file_name
         file_path = os.path.join(directory, file_name)
