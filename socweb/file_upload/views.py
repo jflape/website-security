0919d7cd941dc015e36be113c5bce4d7a5cc8354
jflape <jflape@davao-water.gov.ph>
2023-06-23 13:54:49 +0800

    23JunV1

--- socweb/file_upload/views.py
+++ socweb/file_upload/views.py
@@ -2,39 +2,45 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import pandas as pd
 from datetime import datetime
+import os
 
 def upload_file(request):
     df = []
     if request.method == 'POST':
         file = request.FILES['file']
-        directory = '/usr/local/src/gitrepos/python-web/security/socweb/output/'
+        directory = '../output/'
+
         df.append(pd.read_excel(file, usecols=['IP Address']))
         df_data = pd.concat(df, axis=0)
-        # df_data.to_excel('trickbot.xlsx', index=False, header=False)
-        # df_data.to_csv('trickbot.txt', index=False, header=False)
-        # df_data.to_html('trickbot.html', index=False, header=False)
        
         df_txtoutput = df_data.to_string(index=False, header=False).strip()
         #current_date = datetime.now().strftime("%Y-%m-%d")
         #file_name = f"output_{current_date}.txt"
         file_name = "output.txt"
-        file_path = directory + file_name
+        #file_path = directory + file_name
+        
+        file_path = os.path.join(directory, file_name)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+            
         with open(file_path, 'a') as f:
             f.write(df_txtoutput + '\n')
         
         with open(file_path, 'r') as showdata:
             contents = showdata.read()
-        # df = pd.read_excel(file)
-        # columns = df.columns
+
         return render(request, 'file_upload/result.html', {'display': contents})
     return render(request, 'file_upload/upload.html')
 
 def output_data(request):
-      directory = '/usr/local/src/gitrepos/python-web/security/socweb/output/'
-      file_name = "output.txt"
-      file_path = directory + file_name
+        directory = '../output/'
+        file_name = "output.txt"
+    #   file_path = directory + file_name
+        file_path = os.path.join(directory, file_name)
+        if not os.path.exists(directory):
+            os.makedirs(directory)   
       
-      with open(file_path, 'r') as showdata:
-         content = showdata.read()      
+        with open(file_path, 'r') as showdata:
+            content = showdata.read()      
         
-      return HttpResponse(content, content_type='text/plain')
+        return HttpResponse(content, content_type='text/plain')
