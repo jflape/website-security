c53ca45717d6a9c957c459c216d15171b0ec20fd
jflape <jflape@davao-water.gov.ph>
2023-08-25 15:38:04 +0800

    25Aug23

--- git_update_websec.sh
+++ git_update_websec.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 reponame="website-security"
 home_dir=/usr/local/src
-git_dir=$home_dir/gitrepos
+git_dir=$home_dir/repository
 #file=$home_dir/repolist.txt
 comments="$(date +%d%b%y)"
 #while read -r reponame;
 #do
-    echo "*****Updating $reponame from server to git*****"
+    echo "*****Updating Repository $reponame*****"
     cd $git_dir/$reponame
+    git branch -M main
     git add . 
     git commit -a -m "$comments"
     git push origin main
+    echo "*****Repository $reponame Updated*****"
 #done < "$file"
